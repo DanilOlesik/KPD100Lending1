9647eea152c70a7cc340e5b61206d3d1815d4984
Danil Olesik <danil0077@bk.ru>
2024-10-06 20:13:33 +1000

    ls5

--- scripts/kpd.js
+++ scripts/kpd.js
@@ -1,13 +1,14 @@
 const images = [
-  "/KPD100Lending1/img/mainImg/1.webp",
-  "/KPD100Lending1/img/mainImg/2.webp",
-  "/KPD100Lending1/img/mainImg/3.webp",
-  "/KPD100Lending1/img/mainImg/4.webp",
-  "/KPD100Lending1/img/mainImg/5.webp",
-  "/KPD100Lending1/img/mainImg/6.webp",
+  "/img/mainImg/1.webp",
+  "/img/mainImg/2.webp",
+  "/img/mainImg/3.webp",
+  "/img/mainImg/4.webp",
+  "/img/mainImg/5.webp",
+  "/img/mainImg/6.webp",
 ];
 
 let currentIndex = 0;
+let sliderInterval; // Переменная для хранения интервала
 const container = document.querySelector(".container");
 
 function changeBackground() {
@@ -15,5 +16,26 @@ function changeBackground() {
   container.style.backgroundImage = `url(${images[currentIndex]})`;
 }
 
-// Меняем изображение каждые 3 секунды
-setInterval(changeBackground, 10000);
+// Функция для запуска или остановки слайдера
+function startSlider() {
+  // Если ширина экрана >= 768px, запускаем слайдер
+  if (window.innerWidth >= 768) {
+    // Проверяем, запущен ли уже слайдер
+    if (!sliderInterval) {
+      sliderInterval = setInterval(changeBackground, 10000);
+    }
+  } else {
+    // Останавливаем слайдер на мобильных устройствах и устанавливаем изображение для мобилок
+    clearInterval(sliderInterval);
+    sliderInterval = null; // Сбрасываем переменную
+    container.style.backgroundImage = `url(/KPD100Lending1/img/fon-mobile.png)`; // Устанавливаем мобильное изображение
+  }
+}
+
+// Запускаем слайдер при первой загрузке страницы
+startSlider();
+
+// Обновляем слайдер при изменении размера экрана
+window.addEventListener("resize", () => {
+  startSlider();
+});
